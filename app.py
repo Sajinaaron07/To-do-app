21c4b320cdbf46c94fade57402e626c7151251a9
sajin <sajinaaron777@gmail.com>
2022-07-24 13:15:36 +0530

    init app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+#halo
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -8,7 +10,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    Content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -19,7 +21,7 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['Content']
-        new_task = Todo(content=task_content)
+        new_task = Todo(Content=task_content)
 
         try:
             db.session.add(new_task)
